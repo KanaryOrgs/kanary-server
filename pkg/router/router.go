d4c17ca00ff2b3bb22281506201daa58e4fa2626
geunwoo <gw.noh@alpacanetworks.com>
2024-05-23 19:10:11 +0900

    Add router for Ingress & Service

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -2,20 +2,28 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/kanaryorgs/kanary-server/docs"
 	"github.com/kanaryorgs/kanary-server/pkg/config"
 	"github.com/kanaryorgs/kanary-server/pkg/controllers"
 	"github.com/kanaryorgs/kanary-server/pkg/k8s"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func NewRouter(kh *k8s.K8sHandler) *gin.Engine {
 	router := gin.New()
+	docs.SwaggerInfo.BasePath = "/v1"
+	docs.SwaggerInfo.Title = "kanary-server API"
 	router.Use(gin.LoggerWithFormatter(config.LoggerFormatter))
 	router.Use(gin.Recovery())
 
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	v1 := router.Group("/v1")
 	{
 		setUpPodRoutes(v1, kh)
 		setUpNodeRoutes(v1, kh)
+		setUpServiceRoutes(v1, kh)
+		setUpIngressRoutes(v1, kh)
 	}
 
 	return router
@@ -30,9 +38,25 @@ func setUpPodRoutes(api *gin.RouterGroup, kh *k8s.K8sHandler) {
 }
 
 // setUpNodeRoutes sets up routing for node related endpoints.
-// /v1/pods/
+// /v1/nodes/
 func setUpNodeRoutes(api *gin.RouterGroup, kh *k8s.K8sHandler) {
 	nodeController := controllers.NewNodeController(kh)
 
 	api.GET("/nodes", nodeController.GetNodes)
+}
+
+// setUpServiceRoutes sets up routing for service related endpoints.
+// /v1/services/
+func setUpServiceRoutes(api *gin.RouterGroup, kh *k8s.K8sHandler) {
+	serviceController := controllers.NewServiceController(kh)
+
+	api.GET("/services", serviceController.GetServices)
+}
+
+// setUpIngressRoutes sets up routing for ingress related endpoints.
+// /v1/ingress/
+func setUpIngressRoutes(api *gin.RouterGroup, kh *k8s.K8sHandler) {
+	ingressController := controllers.NewIngressController(kh)
+
+	api.GET("/ingresses", ingressController.GetIngresses)
 }
